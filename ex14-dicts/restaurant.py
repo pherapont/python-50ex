6dabdf67ff09fb0eac290a69e315455fcc831767
pherapont <pherapont@yandex.ru>
2025-08-21 19:54:27 +0300

    первое решение restaurant

--- ex14-dicts/restaurant.py
+++ ex14-dicts/restaurant.py
@@ -5,3 +5,27 @@
 4. Если блюда нет в меню изивиниться и предложить продолжить
 5. Окончание по вводу пустой строки
 '''
+
+from menu import MENU
+
+def menu_response(req: str) -> tuple[int, str]:
+    price = MENU.setdefault(req, 0)
+    # price = MENU[req] if req in MENU else 0
+    ans = str(price) if price else 'Такого блюда нет в меню.'
+    return (price, ans)  
+
+def user_communication():
+    sum = 0
+    while order := input('Заказ: '):
+        resp = menu_response(order)
+        sum += resp[0]
+        if resp[0]:
+            print(f"{order} стоит {resp[0]}, общая сумма {sum}")
+        else:
+            print(resp[1], order)
+    print('Общая сумма:', sum)
+    print('Спасибо за заказ!')
+
+if __name__ == '__main__':
+    user_communication()
+            
--- ex14-dicts/restaurant_test.py
+++ ex14-dicts/restaurant_test.py
@@ -1,5 +1,14 @@
 import unittest
 from menu import MENU
+from restaurant import menu_response
 
 class TestRestaurantMenu(unittest.TestCase):
-    ...
+    def test_dish_on_menu(self):
+        self.assertEqual(menu_response('Салат свекольный'), (55, '55'))
+
+    def test_dish_not_on_menu(self):
+        self.assertEqual(menu_response('Шампанское'),
+                         (0, 'Такого блюда нет в меню.'))
+        
+if __name__ == '__main__':
+    unittest.main()
