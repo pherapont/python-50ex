c6e00429811f3c6a4f3b8bbec8b8b7abe0ead70e
pherapont <pherapont@yandex.ru>
2025-09-04 23:18:02 +0700

    prepare the test

--- ex18-file_last_line/last_line_test.py
+++ ex18-file_last_line/last_line_test.py
@@ -3,3 +3,17 @@
 записываем в него несколько строк, потом применяем к нему get_final_line, 
 и в результате должны получить последнюю строку.
 '''
+
+strings = [
+        'My little friend.',
+        'I want to send your',
+        'it little present.',
+        ]
+
+test_file = 'test_lines.txt'
+
+with open(test_file, 'w') as tf:
+    for line in strings:
+        tf.write(line)
+        tf.write('\n')
+
